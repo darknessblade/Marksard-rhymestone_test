10503700ea9fd8c4f1bb37452a34c481bab065ef
darknessblade <darknessbladers@gmail.com>
2022-09-27 12:12:55 +0200

    Add files via upload

---
+++ keyboards/marksard/rhymestone - Vial test/keymaps/vial/config.h
@@ -0,0 +1,32 @@
+/* Copyright 2020 marksard
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+// place overrides here
+
+// If you use the HashTwenty(alpha), please enable USE_HASHTWENTY
+// #define USE_HASHTWENTY
+
+// If you plug in the USB on the right side, please enable MASTER_RIGHT
+// #define MASTER_RIGHT
+
+#define OLED_FONT_H "keyboards/marksard/rhymestone/common/glcdfont.c"
+
+//VIAL_KEYBOARD_UID
+#define VIAL_KEYBOARD_UID {0xA7, 0xBA, 0x19, 0x62, 0x4B, 0x8B, 0x41, 0x52}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 9 }
---
+++ keyboards/marksard/rhymestone - Vial test/keymaps/vial/rules.mk
@@ -0,0 +1,12 @@
+MOUSEKEY_ENABLE = yes       # Mouse keys
+TAP_DANCE_ENABLE = no
+
+RGBLIGHT_ENABLE = NO        # Enable keyboard RGB underglow
+OLED_ENABLE = YES
+LTO_ENABLE = YES
+VIA_ENABLE = YES
+vial_enable = YES
+VIAL_INSECURE = no
+
+# If you want to change the display of OLED, you need to change here
+SRC +=  ./common/oled_helper.c \
